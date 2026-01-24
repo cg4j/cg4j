c235442993a9ecc726c6e93aeb10bb6af6d54df1
Amir M. Mir <mir-am@hotmail.com>
2026-01-24 15:46:49 +0100

    Add short commit hash to the project version

---
+++ src/main/java/net/cg4j/VersionProvider.java
@@ -0,0 +1,14 @@
+package net.cg4j;
+
+import picocli.CommandLine.IVersionProvider;
+
+/**
+ * Picocli version provider that returns the full version string with git commit hash.
+ */
+public class VersionProvider implements IVersionProvider {
+
+  @Override
+  public String[] getVersion() {
+    return new String[] { Version.getInstance().getFullVersion() };
+  }
+}
